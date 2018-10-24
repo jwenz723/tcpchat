eae00190031713d6eaf097bc1df45d16ac4681cd
jwenz723 <jeff.wenzbauer@incontact.com>
2018-10-24 16:15:10 -0600

    cleaned up tests script

--- ci/tests.sh
+++ ci/tests.sh
@@ -1,19 +1,10 @@
 #!/bin/sh
 
-#cd "$(dirname "$0")/../.."
-#echo "Current dir: $(pwd)"
-#ls
-##go get -d -v
-##ls -Rla $GOPATH
-#mkdir -p $GOPATH/src/github.com/jwenz723/telchat
-#mv ./telchat $GOPATH/src/github.com/jwenz723/telchat
-#cd $GOPATH/src/github.com/jwenz723/telchat
-#go test ./...
-
+# This file taken from: https://github.com/JeffDeCola/hello-go
 set -e -x
 
-# The code is located in /hello-go
-# /coverage-results already created becasue of yml file
+# The code is located in /telchat
+# /coverage-results already created because of yml file
 echo "pwd is: " $PWD
 echo "List whats in the current directory"
 ls -lat
@@ -26,8 +17,6 @@ mkdir -p src/github.com/jwenz723/
 cp -R ./telchat src/github.com/jwenz723/.
 
 # All set and everything is in the right place for go
-echo "Gopath is: " $GOPATH
-echo "pwd is: " $PWD
 cd src/github.com/jwenz723/telchat
 ls -lat
 
